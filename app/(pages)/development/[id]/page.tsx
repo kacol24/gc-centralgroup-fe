fcdc3e2ff4830332f54ecb7c73b8165225e2f7b2
Yasa Kafi Razzan <114861219+YasaKafi@users.noreply.github.com>
2025-02-20 13:32:49 +0700

    feat : [YK] create development detail

--- app/(pages)/development/[id]/page.tsx
+++ app/(pages)/development/[id]/page.tsx
@@ -1,15 +1,30 @@
 'use client';
 
+import { useMemo } from 'react';
 // import { useParams } from 'next/navigation';
 import HeroDetailDevelopment from './components/hero-detail-dev';
+import dynamic from 'next/dynamic';
+import CarouselDetailDevelopment from './components/slider-detail-dev';
+import CardListDetailDevelopment from './components/card-list-detail-dev';
 
 export default function DevelopmentDetailPage() {
   // const params = useParams();
   // const { id } = params;
+  const CoreDetailDevelopment = useMemo(
+    () =>
+      dynamic(() => import('./components/core-detail-dev'), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false,
+      }),
+    [],
+  );
 
   return (
-    <section className="w-screen h-screen flex items-start">
+    <section className="w-screen h-auto bg-backgroundWhite">
       <HeroDetailDevelopment />
+      <CoreDetailDevelopment />
+      <CarouselDetailDevelopment />
+      <CardListDetailDevelopment />
     </section>
   );
 }
