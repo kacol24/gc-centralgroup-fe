4df859ac3149044fa9b52b6ffb40450851fb6a5e
Yasa Kafi Razzan <114861219+YasaKafi@users.noreply.github.com>
2025-02-24 22:40:52 +0700

    feat : [YK] add pagination

--- app/(pages)/article/components/core-article.tsx
+++ app/(pages)/article/components/core-article.tsx
@@ -1,5 +1,16 @@
+'use client';
+
 import { ComboboxDemo } from '@/components/ui/combobox';
+import { useState } from 'react';
 import { RiBuildingFill } from 'react-icons/ri';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination';
 import NewsCard from './card-article';
 import { newsCards } from '@/app/lib/utils/article';
 
@@ -22,9 +33,33 @@ const propertyTypes = [
   },
 ];
 
+const itemsPerPage = 6;
+
 export default function ArticleCore() {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(newsCards.length / itemsPerPage);
+
+  // Hitung indeks item yang akan ditampilkan pada halaman saat ini
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentNews = newsCards.slice(startIndex, endIndex);
+
+  // const handleNextPage = () => {
+  //   if (currentPage < totalPages) {
+  //     setCurrentPage(currentPage + 1);
+  //   }
+  // };
+
+  // const handlePreviousPage = () => {
+  //   if (currentPage > 1) {
+  //     setCurrentPage(currentPage - 1);
+  //   }
+  // };
+
   return (
     <section className="w-full lg:container lg:mx-auto bg-white px-4 pb-8">
+      {/* Header */}
       <div className="w-full flex flex-col lg:flex-row lg:justify-between items-center">
         <h1 className="text-[28px] font-marcellus uppercase text-textPrimary">News & Update</h1>
         <div className="w-full lg:w-auto">
@@ -44,17 +79,58 @@ export default function ArticleCore() {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-8">
-        {newsCards.map((news, index) => (
+        {currentNews.map((news, index) => (
           <NewsCard
             key={index}
             title={news.title}
             description={news.description}
             author={news.author}
-            category={news.date}
+            category={news.category}
             date={news.date}
             image={news.image}
           />
         ))}
+      </div>
+
+      {/* Pagination */}
+      <div className="mt-6 flex justify-center">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                className={
+                  currentPage === 1
+                    ? 'opacity-50 cursor-not-allowed bg-white text-textPrimary shadow-lg border-[#F1F1F1] border-2 hover:bg-primary'
+                    : 'bg-white text-textPrimary shadow-lg border-[#F1F1F1] border-2 hover:bg-primary hover:text-white'
+                }
+              />
+            </PaginationItem>
+
+            {Array.from({ length: totalPages }, (_, i) => (
+              <PaginationItem key={i}>
+                <PaginationLink
+                  isActive={currentPage === i + 1}
+                  onClick={() => setCurrentPage(i + 1)}
+                  className="h-11 w-11 rounded-full border-[#F1F1F1] border-2 hover:bg-primary hover:text-white"
+                >
+                  {i + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+
+            <PaginationItem>
+              <PaginationNext
+                onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+                className={
+                  currentPage === totalPages
+                    ? 'opacity-50 cursor-not-allowed bg-white text-textPrimary shadow-lg border-[#F1F1F1] border-2 hover:bg-primary'
+                    : 'bg-white text-textPrimary shadow-lg border-[#F1F1F1] border-2 hover:bg-primary hover:text-white'
+                }
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </div>
     </section>
   );
