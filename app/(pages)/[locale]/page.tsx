ae14227e76af4ed2302a26a51d84ec8fa3e1181b
Kevin Chandra <kacol.bot@gmail.com>
2025-03-17 18:42:57 +0700

    parallel fetching on homepage

--- app/(pages)/[locale]/page.tsx
+++ app/(pages)/[locale]/page.tsx
@@ -14,22 +14,42 @@ import BannersQuery from '@/graphql/BannersQuery.graphql';
 import BlogsQuery from '@/graphql/BlogsQuery.graphql';
 import {getLocale} from "next-intl/server";
 
-export default async function Home() {
-    const client = await getClient();
-    const locale = await getLocale();
-
+async function getHomepageBanners (locale, client) {
     const {data: heroBanners} = await client.query(BannersQuery, {
         "lang": locale,
         "type": "homepage_banner"
     });
+
+    return heroBanners;
+}
+
+async function getAwardsBanners (locale, client) {
     const {data: awardBanners} = await client.query(BannersQuery, {
         "lang": locale,
         "type": "award_banner"
     });
+
+    return awardBanners;
+}
+
+async function getBlogs (locale, client) {
     const {data: blogs} = await client.query(BlogsQuery, {
         "lang": locale,
         "limit": 6
     });
+
+    return blogs;
+}
+
+export default async function Home() {
+    const client = await getClient();
+    const locale = await getLocale();
+
+    const bannersData = getHomepageBanners(locale, client);
+    const awardsBannersData = getAwardsBanners(locale, client);
+    const blogsData = getBlogs(locale, client);
+
+    const [heroBanners, awardBanners, blogs] = await Promise.all([bannersData, awardsBannersData, blogsData]);
 
   return (
     <div className="bg-backgroundWhite">
