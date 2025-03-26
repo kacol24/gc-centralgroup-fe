e53e1d0bdeb5a2c65bb7b187bceca4bb2a07aa3c
Kevin Chandra <kacol.bot@gmail.com>
2025-03-26 16:07:36 +0700

    development page use map

--- app/(pages)/[locale]/development/page.tsx
+++ app/(pages)/[locale]/development/page.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import PropertyFinder from "./components/property-finder";
 import {getLocale} from "next-intl/server";
 import {getClient} from "@/app/lib/urqlClient";
+import PropertyFinderSection from "@/app/(pages)/[locale]/development/components/property-finder-section";
 
 export default async function Development() {
   const locale = await getLocale();
@@ -37,15 +38,7 @@ export default async function Development() {
         <Suspense>
             <CarouselOurPartner/>
         </Suspense>
-        <section className="w-full h-auto lg:h-[740px] lg:flex">
-            {/* Property Maps */}
-            <div className="w-full h-[295px] lg:h-full lg:flex-grow relative">
-                <Image src={imgPropertyFinderMap} alt="maps" fill/>
-            </div>
-
-            {/* Property Form */}
-            <PropertyFinder/>
-        </section>
+        <PropertyFinderSection />
     </div>
   );
 }
