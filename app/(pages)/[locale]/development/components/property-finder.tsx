45c12cae8f29e8260035e85bafd4014bacb8c1be
madacoda <hamada.undetected@gmail.com>
2025-04-25 14:23:48 +0700

    chore: try to fix loop call query problem

--- app/(pages)/[locale]/development/components/property-finder.tsx
+++ app/(pages)/[locale]/development/components/property-finder.tsx
@@ -8,19 +8,29 @@ import { ComboboxDemo } from '@/components/ui/combobox';
 import { Label } from '@/components/ui/label';
 import { Checkbox } from '@/components/ui/checkbox';
 
-import {useQuery} from "@urql/next";
+import { useQuery } from '@urql/next';
 import PropertyTypesQuery from '@/graphql/PropertyTypesQuery.graphql';
 import LocationsQuery from '@/graphql/LocationsQuery.graphql';
 import FacilitiesQuery from '@/graphql/FacilitiesQuery.graphql';
-import {useSearchParams} from "next/navigation";
-import {useRouter} from '@/i18n/navigation';
-import {useLocale} from "next-intl";
+import { useSearchParams } from 'next/navigation';
+import { useRouter } from '@/i18n/navigation';
+import { useLocale } from 'next-intl';
+import { useMemo } from 'react';
+
+type PropertyFinderProps = {
+  compact?: boolean;
+
+  propertyTypes: {
+    id: string;
+    title: string;
+  }[];
+};
 
 function formatRupiah(value: number) {
   return value >= 1000 ? `Rp ${value / 1000} M` : `Rp ${value} Jt`;
 }
 
-export default function PropertyFinder({ compact = false }) {
+export default function PropertyFinder({ compact = false }: PropertyFinderProps) {
   const locale = useLocale();
   const searchParams = useSearchParams();
   const router = useRouter();
@@ -35,135 +45,139 @@ export default function PropertyFinder({ compact = false }) {
     );
   };
 
-  const [{data: propertyTypesResponse}] = useQuery({
+  const queryVariables = useMemo(() => ({ lang: locale }), [locale]);
+
+  const [{ data: propertyTypesResponse }] = useQuery({
     query: PropertyTypesQuery,
-    variables: {
-      lang: locale
-    }
+    variables: queryVariables,
   });
+  const propertyTypes = useMemo(() => {
+    return (
+      propertyTypesResponse?.propertytypes?.map((propertyType) => ({
+        value: propertyType.id,
+        label: propertyType.title,
+      })) ?? []
+    );
+  }, [propertyTypesResponse]);
 
-  const propertyTypes = propertyTypesResponse.propertytypes.map(propertyType => {
-    return {
-      value: propertyType.id,
-      label: propertyType.title
-    }
-  })
-
-  const [{data: locationsResponse}] = useQuery({
-    query: LocationsQuery
+  const [{ data: locationsResponse }] = useQuery({
+    query: LocationsQuery,
   });
+  const cities = useMemo(() => {
+    return (
+      locationsResponse?.locations?.map((location) => ({
+        value: location.id,
+        label: location.title,
+      })) ?? []
+    );
+  }, [locationsResponse]);
 
-  const cities = locationsResponse.locations.map(location => {
-    return {
-      value: location.id,
-      label: location.title
-    }
-  });
-
-  const [{data: facilitiesResponse}] = useQuery({
+  const [{ data: facilitiesResponse }] = useQuery({
     query: FacilitiesQuery,
-    variables: {
-        lang: locale
-    }
+    variables: queryVariables,
   });
   const facilities = facilitiesResponse.facilities;
 
   const handleFindProperty = () => {
-      const params = new URLSearchParams();
-      if (filterLocation) {
-          params.set('location', filterLocation);
-      }
-      if (filterPropertyType) {
-          params.set('property_type', filterPropertyType);
-      }
-      if (selectedFacilities.length) {
-          params.set('facilities', selectedFacilities.join(','))
-      }
-      if (value) {
-          params.set('price', `${value[0]}-${value[1]}`);
-      }
+    const params = new URLSearchParams();
+    if (filterLocation) {
+      params.set('location', filterLocation);
+    }
+    if (filterPropertyType) {
+      params.set('property_type', filterPropertyType);
+    }
+    if (selectedFacilities.length) {
+      params.set('facilities', selectedFacilities.join(','));
+    }
+    if (value) {
+      params.set('price', `${value[0]}-${value[1]}`);
+    }
 
-      router.push('/search?' + params.toString());
+    router.push('/search?' + params.toString());
   };
 
   return (
-      <div className={`w-full lg:w-[580px] p-8 lg:p-${compact ? 10 : 20} bg-[#2E2E2E] text-white ${compact ? 'lg:min-w-[405px] lg:max-w-[405px]' : ''}`}>
-        <h2 className="text-2xl font-marcellus text-start  mb-10">PROPERTY FINDER</h2>
+    <div
+      className={`w-full lg:w-[580px] p-8 lg:p-${compact ? 10 : 20} bg-[#2E2E2E] text-white ${compact ? 'lg:min-w-[405px] lg:max-w-[405px]' : ''}`}
+    >
+      <h2 className="text-2xl font-marcellus text-start  mb-10">PROPERTY FINDER</h2>
 
-        <div className="mb-[22px]">
-          <ComboboxDemo
-              dataPropertys={cities}
-              placeholder="Location"
-              icon={<FaMapMarkerAlt className="text-white"/>}
-              onValueChange={value => setFilterLocation(value)}
-              defaultValue={searchParams.get('location')}
-              customClassName={{
-                button: 'bg-black text-white hover:bg-black hover:opacity-80 py-6',
-                popoverContent: 'bg-gray-800 text-white',
-                input: 'border-gray-400',
-                item: 'text-gray-700',
-                itemActive: 'bg-blue-300 text-black',
-              }}
-          />
-        </div>
-
-        <div className="mb-6">
-          <ComboboxDemo
-              dataPropertys={propertyTypes}
-              placeholder="Property Types"
-              icon={<RiBuildingFill className="text-white"/>}
-              onValueChange={value => setFilterPropertyType(value)}
-              defaultValue={searchParams.get('property_type')}
-              customClassName={{
-                button: 'bg-black text-white hover:bg-black hover:opacity-80 py-6',
-                popoverContent: 'bg-gray-800 text-white',
-                input: 'border-gray-400',
-                item: 'text-gray-700',
-                itemActive: 'bg-blue-300 text-black',
-              }}
-          />
-        </div>
-
-        <div className="mb-6">
-          <label className="block text-[10px] font-semibold mb-5 uppercase">Price Range</label>
-
-          <Slider
-              value={value}
-              onValueChange={(val) => setValue([val[0], val[1]])}
-              min={0}
-              max={5000}
-              step={100}
-              className="w-full "
-          />
-          <div className="flex justify-between text-white text-sm mt-4">
-            <span>{formatRupiah(value[0])}</span>
-            <span>{formatRupiah(value[1])}</span>
-          </div>
-        </div>
-
-        {/* Facilities Section */}
-        <div className="mb-8 ">
-          <label className="block text-[10px] uppercase font-semibold mb-4">Facilities</label>
-          <div className="flex flex-col gap-4">
-            {facilities.map((facility) => (
-                <div key={facility.id} className="flex items-center space-x-2 ">
-                  <Checkbox
-                      id={facility.id}
-                      checked={selectedFacilities.includes(facility.id)}
-                      onCheckedChange={() => toggleFacility(facility.id)}
-                  />
-                  <Label htmlFor={facility.title} className="text-xs">
-                    {facility.title}
-                  </Label>
-                </div>
-            ))}
-          </div>
-        </div>
-
-        <button className="w-full bg-primary py-4 px-[92px] rounded-sm mt-0 text-xs font-semibold"
-                onClick={handleFindProperty}>
-          FIND PROPERTY
-        </button>
+      <div className="mb-[22px]">
+        <ComboboxDemo
+          dataPropertys={cities}
+          placeholder="Location"
+          icon={<FaMapMarkerAlt className="text-white" />}
+          onValueChange={(value) => setFilterLocation(value)}
+          defaultValue={searchParams.get('location')}
+          customClassName={{
+            button: 'bg-black text-white hover:bg-black hover:opacity-80 py-6',
+            popoverContent: 'bg-gray-800 text-white',
+            input: 'border-gray-400',
+            item: 'text-gray-700',
+            itemActive: 'bg-blue-300 text-black',
+          }}
+        />
       </div>
+
+      <div className="mb-6">
+        <ComboboxDemo
+          dataPropertys={propertyTypes}
+          placeholder="Property Types"
+          icon={<RiBuildingFill className="text-white" />}
+          onValueChange={(value) => setFilterPropertyType(value)}
+          defaultValue={searchParams.get('property_type')}
+          customClassName={{
+            button: 'bg-black text-white hover:bg-black hover:opacity-80 py-6',
+            popoverContent: 'bg-gray-800 text-white',
+            input: 'border-gray-400',
+            item: 'text-gray-700',
+            itemActive: 'bg-blue-300 text-black',
+          }}
+        />
+      </div>
+
+      <div className="mb-6">
+        <label className="block text-[10px] font-semibold mb-5 uppercase">Price Range</label>
+
+        <Slider
+          value={value}
+          onValueChange={(val) => setValue([val[0], val[1]])}
+          min={0}
+          max={5000}
+          step={100}
+          className="w-full "
+        />
+        <div className="flex justify-between text-white text-sm mt-4">
+          <span>{formatRupiah(value[0])}</span>
+          <span>{formatRupiah(value[1])}</span>
+        </div>
+      </div>
+
+      {/* Facilities Section */}
+      <div className="mb-8 ">
+        <label className="block text-[10px] uppercase font-semibold mb-4">Facilities</label>
+        <div className="flex flex-col gap-4">
+          {facilities.map((facility) => (
+            <div key={facility.id} className="flex items-center space-x-2 ">
+              <Checkbox
+                id={facility.id}
+                checked={selectedFacilities.includes(facility.id)}
+                onCheckedChange={() => toggleFacility(facility.id)}
+              />
+              <Label htmlFor={facility.title} className="text-xs">
+                {facility.title}
+              </Label>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <button
+        className="w-full bg-primary py-4 px-[92px] rounded-sm mt-0 text-xs font-semibold"
+        onClick={handleFindProperty}
+      >
+        FIND PROPERTY
+      </button>
+    </div>
   );
 }
