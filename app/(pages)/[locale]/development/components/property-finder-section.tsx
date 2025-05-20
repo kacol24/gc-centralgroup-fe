854366fb85516768e0cc6f9a8d63210c700f6391
Kevin Chandra <kacol.bot@gmail.com>
2025-05-20 16:33:03 +0700

    Revert "disable map to debug looping issue"
    This reverts commit df1d3bab9b62749b4738b03e71efba8678d049e1.

--- app/(pages)/[locale]/development/components/property-finder-section.tsx
+++ app/(pages)/[locale]/development/components/property-finder-section.tsx
@@ -1,23 +1,23 @@
 'use client';
 
 import PropertyFinder from '@/app/(pages)/[locale]/development/components/property-finder';
-// import { useMemo } from 'react';
-// import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
+import dynamic from 'next/dynamic';
 
 export default function PropertyFinderSection() {
-  // const PropertyMap = useMemo(
-  //   () =>
-  //     dynamic(() => import('./property-map'), {
-  //       loading: () => <p>A map is loading</p>,
-  //       ssr: false,
-  //     }),
-  //   [],
-  // );
+  const PropertyMap = useMemo(
+    () =>
+      dynamic(() => import('./property-map'), {
+        loading: () => <p>A map is loading</p>,
+        ssr: false,
+      }),
+    [],
+  );
 
   return (
     <section className="w-full h-auto lg:flex">
       <div className="w-full h-[295px] lg:h-auto lg:flex-grow relative">
-        {/*<PropertyMap />*/}
+        <PropertyMap />
       </div>
       <PropertyFinder compact/>
     </section>
