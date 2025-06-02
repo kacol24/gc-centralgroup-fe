cbeeca13ef9a97ad97959ec26f584bdf6b3b0f3a
madacoda <hamada.undetected@gmail.com>
2025-06-02 11:11:02 +0700

    chore: update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,10 +36,10 @@ export default async function RootLayout({
   return (
     <html lang={locale}>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=202505271754" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=202505271754" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=202505271754" />
-        <link rel="manifest" href="/site.webmanifest?v=202505271754" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=202506021107" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=202506021107" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=202506021107" />
+        <link rel="manifest" href="/site.webmanifest?v=202506021107" />
       </Head>
       <GraphqlProvider token={accessToken}>
         <body
