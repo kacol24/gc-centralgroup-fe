f5595516215eb92cb5e3d6dd3ce4a1cbf03d3fdc
madacoda <hamada.undetected@gmail.com>
2025-06-02 16:53:43 +0700

    chore: integrate contact form

--- app/(pages)/[locale]/contact/page.tsx
+++ app/(pages)/[locale]/contact/page.tsx
@@ -14,6 +14,9 @@ import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import { imgLoadingMaps } from '@/app/lib/utils/image';
 import Image from 'next/image';
+import { Link } from '@/i18n/navigation';
+import ContactForm from '@/graphql/ContactForm.graphql';
+import { useMutation } from '@urql/next';
 
 export default function Contact() {
   const MapContact = useMemo(
@@ -35,6 +38,48 @@ export default function Contact() {
       startEvent: 'DOMContentLoaded',
     });
   }, []);
+
+  const [, contactForm] = useMutation(ContactForm);
+
+  const handleContactForm = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const phone = form.phone.value;
+    const email = form.email.value;
+    const message = form.message.value;
+
+    try {
+      const res = await contactForm({
+        name: name,
+        phone: phone,
+        email: email,
+        message: message,
+      });
+
+      if (res.error) {
+        const validationErrors = res.error.graphQLErrors?.[0]?.extensions?.validation;
+        if (validationErrors) {
+          const errors = {};
+          for (const key in validationErrors) {
+            const cleanKey = key.replace(/^input\./, '');
+            const cleanMessages = validationErrors[key].map((msg) => msg.replace(/input\./g, ''));
+            errors[cleanKey] = cleanMessages;
+          }
+          alert(Object.values(errors).flat().join('\n'));
+          return;
+        }
+      }
+
+      if (res?.data.contactForm?.status === 'CONTACT_FORM_SUBMITTED') {
+        alert('Thank you for your message! We will get back to you soon.');
+        form.reset();
+      }
+    } catch (error) {
+      console.error('Error submitting contact form:', error);
+    }
+  };
+
   return (
     <section className="w-full h-auto pt-[170px] lg:pt-[223px] bg-backgroundWhite lg:pb-[90px] pb-0">
       <div className="container mx-auto px-4">
@@ -75,12 +120,16 @@ export default function Contact() {
           >
             <div className="text-white space-y-4">
               <div className="flex items-center gap-3">
-                <IoLogoWhatsapp className="text-base text-textTertiary" />
-                <span>+62 811 703 8868</span>
+                <Link href="https://wa.me/628117038868" target="_blank">
+                  <IoLogoWhatsapp className="text-base text-textTertiary" />
+                  <span>+62 811 703 8868</span>
+                </Link>
               </div>
               <div className="flex items-center gap-3">
-                <FaEnvelope className="text-base text-textTertiary" />
-                <span>info@centralgroup.id</span>
+                <Link href="mailto:info@centralgroup.id" target="_blank">
+                  <FaEnvelope className="text-base text-textTertiary" />
+                  <span>info@centralgroup.id</span>
+                </Link>
               </div>
             </div>
 
@@ -111,112 +160,14 @@ export default function Contact() {
         </div>
         <MapContact />
 
-        <div className="w-full flex-col flex xl:hidden xl:flex-col ">
-          <div className="p-12 bg-white shadow-xl rounded-sm">
-            <h1 className="font-marcellus text-textPrimary text-2xl uppercase mb-8">GEt In touch</h1>
-
-            <div className="space-y-4 mb-6">
-              {/* Property Price */}
-              <div data-aos="zoom-in" data-aos-duration="2000" className="space-y-2">
-                <Label htmlFor="property-price" className="text-[10px] font-semibold text-gray-900">
-                  YOUR NAME
-                </Label>
-                <Input
-                  id="name"
-                  placeholder="Your Name"
-                  style={{
-                    backgroundColor: 'white',
-                    borderColor: '#E1E1E1',
-                    borderRadius: '0px',
-                    fontSize: '12px',
-                    paddingTop: '23px',
-                    paddingBottom: '23px',
-                  }}
-                  className=" text-gray-900"
-                />
-              </div>
-
-              <div data-aos="zoom-in" data-aos-duration="2000" className="space-y-2">
-                <Label htmlFor="down-payment" className="text-[10px] font-semibold text-gray-900">
-                  NOMOR HANDPHONE
-                </Label>
-                <div className="relative">
-                  <span className="absolute flex inset-y-0 left-3  items-center text-black text-[12px] pointer-events-none">
-                    +62
-                  </span>
-                  <Input
-                    id="phone"
-                    placeholder="87654321"
-                    style={{
-                      backgroundColor: 'white',
-                      borderColor: '#E1E1E1',
-                      borderRadius: '0px',
-                      fontSize: '12px',
-                      paddingLeft: '40px',
-                      paddingTop: '23px',
-                      paddingBottom: '23px',
-                    }}
-                    className=" text-gray-900 border border-gray-300 focus:ring-2 focus:ring-gray-400"
-                  />
-                </div>
-              </div>
-
-              <div data-aos="zoom-in" data-aos-duration="2000" className="space-y-2">
-                <Label htmlFor="loan-term" className="text-[10px] font-semibold text-gray-900">
-                  YOUR EMAIL
-                </Label>
-                <Input
-                  id="email"
-                  placeholder="Your Email"
-                  style={{
-                    backgroundColor: 'white',
-                    borderColor: '#E1E1E1',
-                    borderRadius: '0px',
-                    fontSize: '12px',
-                    paddingTop: '23px',
-                    paddingBottom: '23px',
-                  }}
-                  className=" text-gray-900 border border-gray-300 focus:ring-2 focus:ring-gray-400"
-                />
-              </div>
-
-              <div data-aos="zoom-in" data-aos-duration="2000" className="space-y-2">
-                <Label htmlFor="loan-term" className="text-[10px] font-semibold text-gray-900">
-                  YOUR MESSAGE
-                </Label>
-                <Textarea
-                  id="message"
-                  placeholder="Your Message"
-                  style={{
-                    backgroundColor: 'white',
-                    borderColor: '#E1E1E1',
-                    borderRadius: '0px',
-                    fontSize: '12px',
-                  }}
-                  className=" text-gray-900 border border-gray-300 focus:ring-2 focus:ring-gray-400"
-                />
-              </div>
-            </div>
-
-            <Button
-              data-aos="zoom-in"
-              data-aos-duration="1000"
-              variant="filled"
-              className="rounded-none w-full font-medium text-xs py-[24px] uppercase"
-            >
-              Send Message
-            </Button>
-          </div>
-        </div>
-
-        <div className="absolute top-1/2 -translate-y-[60%] right-20 hidden xl:block">
-          <div className="w-[405px] hidden lg:flex lg:flex-col ">
+        <form onSubmit={handleContactForm}>
+          <div className="w-full flex-col flex xl:hidden xl:flex-col ">
             <div className="p-12 bg-white shadow-xl rounded-sm">
               <h1 className="font-marcellus text-textPrimary text-2xl uppercase mb-8">GEt In touch</h1>
 
               <div className="space-y-4 mb-6">
                 {/* Property Price */}
-                <div className="space-y-2">
+                <div data-aos="zoom-in" data-aos-duration="2000" className="space-y-2">
                   <Label htmlFor="property-price" className="text-[10px] font-semibold text-gray-900">
                     YOUR NAME
                   </Label>
@@ -235,12 +186,12 @@ export default function Contact() {
                   />
                 </div>
 
-                <div className="space-y-2">
+                <div data-aos="zoom-in" data-aos-duration="2000" className="space-y-2">
                   <Label htmlFor="down-payment" className="text-[10px] font-semibold text-gray-900">
                     NOMOR HANDPHONE
                   </Label>
                   <div className="relative">
-                    <span className="absolute hidden lg:flex inset-y-0 left-3  items-center text-black text-[12px] pointer-events-none">
+                    <span className="absolute flex inset-y-0 left-3  items-center text-black text-[12px] pointer-events-none">
                       +62
                     </span>
                     <Input
@@ -260,7 +211,7 @@ export default function Contact() {
                   </div>
                 </div>
 
-                <div className="space-y-2">
+                <div data-aos="zoom-in" data-aos-duration="2000" className="space-y-2">
                   <Label htmlFor="loan-term" className="text-[10px] font-semibold text-gray-900">
                     YOUR EMAIL
                   </Label>
@@ -279,7 +230,7 @@ export default function Contact() {
                   />
                 </div>
 
-                <div className="space-y-2">
+                <div data-aos="zoom-in" data-aos-duration="2000" className="space-y-2">
                   <Label htmlFor="loan-term" className="text-[10px] font-semibold text-gray-900">
                     YOUR MESSAGE
                   </Label>
@@ -297,12 +248,123 @@ export default function Contact() {
                 </div>
               </div>
 
-              <Button variant="filled" className="rounded-none w-full font-medium text-xs py-[24px] uppercase">
+              <Button
+                data-aos="zoom-in"
+                data-aos-duration="1000"
+                variant="filled"
+                className="rounded-none w-full font-medium text-xs py-[24px] uppercase"
+                type="submit"
+              >
                 Send Message
               </Button>
             </div>
           </div>
-        </div>
+        </form>
+
+        <form onSubmit={handleContactForm}>
+          <div className="absolute top-1/2 -translate-y-[60%] right-20 hidden xl:block">
+            <div className="w-[405px] hidden lg:flex lg:flex-col ">
+              <div className="p-12 bg-white shadow-xl rounded-sm">
+                <h1 className="font-marcellus text-textPrimary text-2xl uppercase mb-8">GEt In touch</h1>
+
+                <div className="space-y-4 mb-6">
+                  {/* Property Price */}
+                  <div className="space-y-2">
+                    <Label htmlFor="property-price" className="text-[10px] font-semibold text-gray-900">
+                      YOUR NAME
+                    </Label>
+                    <Input
+                      id="name"
+                      placeholder="Your Name"
+                      name="name"
+                      style={{
+                        backgroundColor: 'white',
+                        borderColor: '#E1E1E1',
+                        borderRadius: '0px',
+                        fontSize: '12px',
+                        paddingTop: '23px',
+                        paddingBottom: '23px',
+                      }}
+                      className=" text-gray-900"
+                    />
+                  </div>
+
+                  <div className="space-y-2">
+                    <Label htmlFor="down-payment" className="text-[10px] font-semibold text-gray-900">
+                      NOMOR HANDPHONE
+                    </Label>
+                    <div className="relative">
+                      <span className="absolute hidden lg:flex inset-y-0 left-3  items-center text-black text-[12px] pointer-events-none">
+                        +62
+                      </span>
+                      <Input
+                        id="phone"
+                        placeholder="87654321"
+                        name="phone"
+                        style={{
+                          backgroundColor: 'white',
+                          borderColor: '#E1E1E1',
+                          borderRadius: '0px',
+                          fontSize: '12px',
+                          paddingLeft: '40px',
+                          paddingTop: '23px',
+                          paddingBottom: '23px',
+                        }}
+                        className=" text-gray-900 border border-gray-300 focus:ring-2 focus:ring-gray-400"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="space-y-2">
+                    <Label htmlFor="loan-term" className="text-[10px] font-semibold text-gray-900">
+                      YOUR EMAIL
+                    </Label>
+                    <Input
+                      id="email"
+                      placeholder="Your Email"
+                      name="email"
+                      style={{
+                        backgroundColor: 'white',
+                        borderColor: '#E1E1E1',
+                        borderRadius: '0px',
+                        fontSize: '12px',
+                        paddingTop: '23px',
+                        paddingBottom: '23px',
+                      }}
+                      className=" text-gray-900 border border-gray-300 focus:ring-2 focus:ring-gray-400"
+                    />
+                  </div>
+
+                  <div className="space-y-2">
+                    <Label htmlFor="loan-term" className="text-[10px] font-semibold text-gray-900">
+                      YOUR MESSAGE
+                    </Label>
+                    <Textarea
+                      id="message"
+                      placeholder="Your Message"
+                      name="message"
+                      style={{
+                        backgroundColor: 'white',
+                        borderColor: '#E1E1E1',
+                        borderRadius: '0px',
+                        fontSize: '12px',
+                      }}
+                      className=" text-gray-900 border border-gray-300 focus:ring-2 focus:ring-gray-400"
+                    />
+                  </div>
+                </div>
+
+                <Button
+                  variant="filled"
+                  className="rounded-none w-full font-medium text-xs py-[24px] uppercase"
+                  type="submit"
+                >
+                  Send Message
+                </Button>
+              </div>
+            </div>
+          </div>
+        </form>
       </div>
     </section>
   );
