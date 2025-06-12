53ae5a05e061ff9c2900eaae143c2280f01c4c3a
Kevin Chandra <kacol.bot@gmail.com>
2025-06-12 16:18:46 +0700

    build(deps): install sentry

---
+++ instrumentation-client.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3fa0da29f712441eee5e609f027f3f05@o4507099778187264.ingest.us.sentry.io/4509483669454848",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3fa0da29f712441eee5e609f027f3f05@o4507099778187264.ingest.us.sentry.io/4509483669454848",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
