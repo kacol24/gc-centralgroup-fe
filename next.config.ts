cf575a11f02ac0ee01fcc2406300915befc0db74
Kevin Chandra <kacol.bot@gmail.com>
2025-06-16 10:13:11 +0700

    build: load sentry with no sourcemaps and then intl

--- next.config.ts
+++ next.config.ts
@@ -47,35 +47,41 @@ const nextConfig: NextConfig = {
     },
 };
 
-const withNextIntl = createNextIntlPlugin();
-export default withSentryConfig(withNextIntl(nextConfig), {
+const sentryOptions = {
 // For all available options, see:
 // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-org: "goodcommerce-4g",
-project: "centralgroup-fe",
+    org: "goodcommerce-4g",
+    project: "centralgroup-fe",
 
 // Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+    silent: !process.env.CI,
 
 // For all available options, see:
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
 // Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+    widenClientFileUpload: false,
 
 // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 // This can increase your server load as well as your hosting bill.
 // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
 // side errors will fail.
-tunnelRoute: "/monitoring",
+    tunnelRoute: "/monitoring",
 
 // Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+    disableLogger: true,
 
 // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
 // See the following for more information:
 // https://docs.sentry.io/product/crons/
 // https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+    automaticVercelMonitors: false,
+
+    sourcemaps: {
+        disable: true,
+    },
+};
+
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(withSentryConfig(nextConfig, sentryOptions));
